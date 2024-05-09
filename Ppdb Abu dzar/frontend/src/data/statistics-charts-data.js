a849115d70f7ce93f9738c85bb692bca12012608
rezkiabudzar <rezkisuryana14@gmail.com>
2024-05-09 08:09:47 +0700

    add and adjust card statistics in homepage admin

--- Ppdb Abu dzar/frontend/src/data/statistics-charts-data.js
+++ Ppdb Abu dzar/frontend/src/data/statistics-charts-data.js
@@ -1,5 +1,28 @@
 import { chartsConfig } from "@/configs";
 
+const pieChart = {
+  type: "pie",
+  height: 220,
+  series: [44, 55, 13, 43, 22, 10],
+  options: {
+    ...chartsConfig,
+    colors: ["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3"],
+    labels: ['TK', 'SD', 'SMP', 'SMA', 'Pondok SMP', 'Pondok SMA'],
+    responsive: [
+      {
+        options: {
+          chart: {
+            width: 200,
+          },
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
+    ],
+  }
+}
+
 const websiteViewsChart = {
   type: "bar",
   height: 220,
@@ -25,40 +48,40 @@ const websiteViewsChart = {
   },
 };
 
-const dailySalesChart = {
-  type: "line",
-  height: 220,
-  series: [
-    {
-      name: "Sales",
-      data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
-    },
-  ],
-  options: {
-    ...chartsConfig,
-    colors: ["#0288d1"],
-    stroke: {
-      lineCap: "round",
-    },
-    markers: {
-      size: 5,
-    },
-    xaxis: {
-      ...chartsConfig.xaxis,
-      categories: [
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ],
-    },
-  },
-};
+// const dailySalesChart = {
+//   type: "line",
+//   height: 220,
+//   series: [
+//     {
+//       name: "Sales",
+//       data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
+//     },
+//   ],
+//   options: {
+//     ...chartsConfig,
+//     colors: ["#0288d1"],
+//     stroke: {
+//       lineCap: "round",
+//     },
+//     markers: {
+//       size: 5,
+//     },
+//     xaxis: {
+//       ...chartsConfig.xaxis,
+//       categories: [
+//         "Apr",
+//         "May",
+//         "Jun",
+//         "Jul",
+//         "Aug",
+//         "Sep",
+//         "Oct",
+//         "Nov",
+//         "Dec",
+//       ],
+//     },
+//   },
+// };
 
 const completedTaskChart = {
   type: "line",
@@ -107,18 +130,25 @@ const completedTasksChart = {
 export const statisticsChartsData = [
   {
     color: "white",
-    title: "Website View",
-    description: "Last Campaign Performance",
-    footer: "campaign sent 2 days ago",
-    chart: websiteViewsChart,
+    title: "Info Kuota",
+    description: "Total Info Keseluruhan",
+    footer: "updated 3 min ago",
+    chart: pieChart,
   },
   {
     color: "white",
-    title: "Daily Sales",
-    description: "15% increase in today sales",
-    footer: "updated 4 min ago",
-    chart: dailySalesChart,
+    title: "Statistik Terakhir",
+    description: "Melihat Statistik terakhir",
+    footer: "campaign sent 2 days ago",
+    chart: websiteViewsChart,
   },
+  // {
+  //   color: "white",
+  //   title: "Statistik Pertahun",
+  //   description: "Melihat Statistik pertahun",
+  //   footer: "updated 4 min ago",
+  //   chart: dailySalesChart,
+  // },
   {
     color: "white",
     title: "Completed Tasks",
