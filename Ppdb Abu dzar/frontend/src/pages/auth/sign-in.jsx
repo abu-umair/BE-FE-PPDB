74e0da86433453c1df39a639c476ec1f9632d473
rezkiabudzar <rezkisuryana14@gmail.com>
2024-06-18 15:11:27 +0700

    adjust ui and typography

--- Ppdb Abu dzar/frontend/src/pages/auth/sign-in.jsx
+++ Ppdb Abu dzar/frontend/src/pages/auth/sign-in.jsx
@@ -67,8 +67,8 @@ export function SignIn() {
       <Toast />
       <div className="w-full lg:w-3/5 mt-24">
         <div className="text-center">
-          <Typography variant="h2" className="font-bold mb-4">Sign In test</Typography>
-          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Enter your email and password to Sign In.</Typography>
+          <Typography variant="h2" className="font-bold mb-4">Masuk akun PPDB</Typography>
+          <Typography variant="paragraph" color="blue-gray" className="text-lg font-normal">Masukan email and password untuk masuk.</Typography>
         </div>
         <Formik
           initialValues={initialValues}
@@ -80,7 +80,7 @@ export function SignIn() {
               <Form className="mt-8 mb-2 mx-auto w-80 max-w-screen-lg lg:w-1/2">
                 <div className="mb-1 flex flex-col gap-6">
                   <Typography variant="small" color="blue-gray" className="-mb-3 font-medium">
-                    Your email
+                    Email
                   </Typography>
                   <Field name="email">
                     {({ field }) => (
@@ -187,9 +187,9 @@ export function SignIn() {
                     <span>Sign in With Twitter</span>
                   </Button>
                 </div> */}
-                <Typography variant="paragraph" className="text-center text-blue-gray-500 font-medium mt-4">
-                  Not registered?
-                  <Link to="/sign-up" className="text-gray-900 ml-1">Create account</Link>
+                <Typography variant="paragraph" className="text-center text-gray-900 font-normal mt-4">
+                  Belum mendaftar?
+                  <Link to="/sign-up" className="text-gray-900 ml-1 font-bold">Buat Akun</Link>
                 </Typography>
               </Form>)
           }}
