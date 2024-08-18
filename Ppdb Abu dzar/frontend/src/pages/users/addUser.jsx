5f3b6f38af89b0b10f0a608a96bfd5a1e512a97d
rezkiabudzar <rezkisuryana14@gmail.com>
2024-08-18 23:21:51 +0700

    fix class double

--- Ppdb Abu dzar/frontend/src/pages/users/addUser.jsx
+++ Ppdb Abu dzar/frontend/src/pages/users/addUser.jsx
@@ -135,7 +135,7 @@ const AddUser = (props) => {
                                         {/* <label className="text-sm font-bold text-gray-500 tracking-wide">Attach Document</label> */}
                                         <div className="flex items-center justify-center w-full">
                                             <label className="flex flex-col rounded-lg border-4 border-dashed w-full h-60 p-10 group text-center">
-                                                <div className="h-full w-full text-center flex flex-col items-center justify-center items-center  ">
+                                                <div className="h-full w-full text-center flex flex-col items-center justify-center">
                                                     <svg xmlns="http://www.w3.org/2000/svg" className="w-10 h-10 text-blue-400 group-hover:text-blue-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                                                     </svg>
