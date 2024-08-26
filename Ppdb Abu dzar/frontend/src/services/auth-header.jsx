7f9f0acdb624d368ee719ea4c66e43b0532a1258
abu-umair <mrdwnw93@gmail.com>
2024-08-26 13:22:27 +0700

    Fix Slow Page

--- Ppdb Abu dzar/frontend/src/services/auth-header.jsx
+++ Ppdb Abu dzar/frontend/src/services/auth-header.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: 'https://beppdb.evolusidigital.id/api/ppdb-abudzar/',
+  // baseURL: 'http://127.0.0.1:8000/api/ppdb-abudzar/',
   headers: {
     'Content-Type': 'application/json',
   },
